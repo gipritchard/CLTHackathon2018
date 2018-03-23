33486f47f52abcbb5e237e8121811c04e073837e
Alex Pritchard <johnalexander.pritchard@duke-energy.com>
2018-03-23 11:23:13 -0400

    added common libraries that we will be using and integrated the app with fabric

---
+++ app/src/main/java/com/theideallab/clthackathon2018/application/HackathonApplication.java
@@ -0,0 +1,22 @@
+package com.theideallab.clthackathon2018.application;
+
+import android.app.Application;
+
+import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.answers.Answers;
+
+import io.fabric.sdk.android.Fabric;
+
+/**
+ * Created by Alex Pritchard on 3/23/18.
+ */
+
+public class HackathonApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Fabric.with(this, new Crashlytics(), new Answers());
+    }
+}
