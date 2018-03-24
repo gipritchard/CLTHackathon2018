ae62eb926e8b247ef254fd080a717781896fec86
Zach Nelson <zach.nelson@duke-energy.com>
2018-03-23 23:43:20 -0400

    model implements heatMapData excuse the ugly

--- app/src/main/java/com/theideallab/clthackathon2018/model/GenericModelNameHere.java
+++ app/src/main/java/com/theideallab/clthackathon2018/model/GenericModelNameHere.java
@@ -1,9 +1,19 @@
 package com.theideallab.clthackathon2018.model;
 
+import android.support.annotation.NonNull;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.maps.android.heatmaps.WeightedLatLng;
+import com.theideallab.clthackathon2018.application.heatmap.HeatMapData;
+
+import java.util.ArrayList;
+
+import javax.annotation.Nonnull;
+
 import io.realm.RealmList;
 import io.realm.RealmObject;
 
-public class GenericModelNameHere extends RealmObject {
+public class GenericModelNameHere extends RealmObject implements HeatMapData {
 
     private RealmList<Coordinate> coordinateList;
     private String filterName;
@@ -16,6 +26,34 @@ public class GenericModelNameHere extends RealmObject {
         this.coordinateList = coordinateList;
     }
 
+    @Override
+    public int getId() {
+        return 0;
+    }
+
+    @NonNull
+    @Override
+    public ArrayList<LatLng> getPoints() {
+        ArrayList<LatLng> retval = new ArrayList<>();
+        for (Coordinate c : coordinateList) {
+            retval.add(new LatLng(c.getLatitutde(), c.getLongitude()));
+        }
+
+        return retval;
+    }
+
+    @NonNull
+    @Override
+    public ArrayList<WeightedLatLng> getWeightedPoints() {
+        return null;
+    }
+
+    @NonNull
+    @Override
+    public String getType() {
+        return filterType;
+    }
+
     public RealmList<Coordinate> getCoordinateList() {
         return coordinateList;
     }
@@ -24,6 +62,8 @@ public class GenericModelNameHere extends RealmObject {
         this.coordinateList = coordinateList;
     }
 
+    @Nonnull
+    @Override
     public String getFilterName() {
         return filterName;
     }
